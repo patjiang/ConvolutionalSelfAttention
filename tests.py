1b6cffda6493c5874a8c2d78e20a3a7869c40669
Patrick Jiang <patjiang04@gmail.com>
2025-08-12 08:00:53 -0700

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,21 @@
+import torch
+from utils.modules import *
+
+
+#Test that Conv3d layer does not change input shape
+input = torch.rand(64, 1, 28, 28, 28)
+
+model = ConvolutionalSelfAttention3d(1, (28*28), 4)
+
+output = model(input)
+assert output.shape == input.shape
+print('conv3d CSA passed')
+
+#Test that Conv2d layer does not change input shape
+input = torch.rand(64, 1, 28, 28)
+
+model = ConvolutionalSelfAttention2d(1, (28*28), 4)
+
+output = model(input)
+assert output.shape == input.shape
+print('conv2d CSA passed')
