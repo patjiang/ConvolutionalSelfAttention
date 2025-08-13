66e986144355c94b1d073d7500f50d35348b9cc1
Patrick Jiang <patjiang04@gmail.com>
2025-08-13 14:58:01 -0700

    Update benchmark_pipe.py

--- utils/benchmark_pipe.py
+++ utils/benchmark_pipe.py
@@ -88,4 +88,4 @@ def run_test(model, trainloader = trainloader, folder = 'lats', nlr = 0.001):
 
   output_path = f'/content/{folder}/vid_out.mp4'
 
-  !ffmpeg -r 10 -i "{image_folder}/frame_%03d.png" -vcodec libx264 -pix_fmt yuv420p "{output_path}" > /dev/null 2>&1
+  os.system('ffmpeg -r 10 -i "{image_folder}/frame_%03d.png" -vcodec libx264 -pix_fmt yuv420p "{output_path}" > /dev/null 2>&1')
