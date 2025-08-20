b098bf34df91dc62c36da4f93e4a1498d87bef55
Patrick Jiang <patjiang04@gmail.com>
2025-08-20 10:31:48 -0700

    Update benchmark_pipe.py

--- utils/benchmark_pipe.py
+++ utils/benchmark_pipe.py
@@ -64,7 +64,6 @@ def run_test(model, trainloader, testloader, folder = 'lats', nlr = 0.001):
         lats, labs = [], []
       for images, labels in (trainloader):
           optimizer.zero_grad()
-
           pred, emb = model(images.to(model.device))
           output = F.log_softmax(pred, dim=1)
           loss = criterion(output, labels.to(model.device))
@@ -77,6 +76,7 @@ def run_test(model, trainloader, testloader, folder = 'lats', nlr = 0.001):
       with torch.no_grad():
         test_run, test_corr = 0, 0
         for images, labels in (testloader):
+          print(images.shape)
           pred, emb = model(images.to(model.device))
           output = F.log_softmax(pred, dim=1)
           loss = criterion(output, labels.to(model.device))
