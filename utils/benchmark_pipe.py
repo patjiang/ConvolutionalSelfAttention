5395359901bc63df99167ea916db11796d82a5a3
Patrick Jiang <patjiang04@gmail.com>
2025-08-20 10:38:07 -0700

    Update benchmark_pipe.py

--- utils/benchmark_pipe.py
+++ utils/benchmark_pipe.py
@@ -90,7 +90,7 @@ def run_test(model, trainloader, testloader, folder = 'lats', nlr = 0.001):
               lats.append(emb.detach().cpu().squeeze().flatten(start_dim=1).numpy())
               labs.append(labels)
       
-      test_accuracy = num_correct.float()/ len(testloader)
+      test_accuracy = test_corr.float()/ len(testloader)
       tlosses.append(test_run/len(testloader))
       taccur.append(test_accuracy)
       losses.append(running_loss/len(trainloader))
