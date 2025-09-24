bdb6d2f9fc9fca0e1fc815f045d3536a70bd2487
Patrick Jiang <patjiang04@gmail.com>
2025-09-24 11:03:24 -0700

    Update modules.py
    Added change mentioned in previous commit

--- utils/modules.py
+++ utils/modules.py
@@ -91,6 +91,65 @@ class ConvolutionalSelfAttention2d(nn.Module):
     qkv = v * qk
     out_feat = self.pw_conv_3(qkv)
     return x * out_feat
+
+class ConvolutionalSelfAttention(nn.Module):
+  def __init__(self, in_ch, dimprod, dims = 2, activation = 'sigmoid', pool = 'avg', scale_reduce = None):
+    super().__init__()
+    if dims == 2:
+      Convolution = nn.Conv2d
+    elif dims == 3:
+      Convolusion = nn.Conv3d
+    self.dw_conv = Convolution(in_ch, in_ch, 5, padding = 2)
+
+    if activation == 'sigmoid':
+      act = nn.Sigmoid()
+    elif activation == 'gelu':
+      act = nn.GELU()
+    elif activation = 'relu':
+      act = nn.ReLU()
+
+    if pool == 'avg':
+      pool = nn.AvgPool2d(scale_reduce)
+    elif pool == 'max':
+      pool = nn.MaxPool2d(scale_reduce)
+    elif pool == 'adavg':
+      pool = nn.AdaptiveAvgPool2d(scale_reduce)
+    elif pool == 'adamax':
+      pool = nn.AdaptiveMaxPool2d(scale_reduce)
+    
+    if(scale_reduce == None):
+      self.pw_conv_1 = Convolution(in_ch, dimprod, 1)
+      self.pw_conv_2 = nn.Sequential(
+          Convolution(dimprod, in_ch, 1),
+          nn.Sigmoid()
+      )
+
+    else:
+      scaled_hw = (dimprod // (scale_reduce**2))
+      self.pw_conv_1 = nn.Sequential(
+          pool,
+          Convolution(in_ch, scaled_hw, 1)
+      )
+      self.pw_conv_2 = nn.Sequential(
+          nn.Convolution(scaled_hw, in_ch, 1),
+          nn.Sigmoid(),
+          nn.Upsample(scale_factor = scale_reduce)
+      )
+    self.pw_conv_3 = nn.Sequential(
+        nn.Convolution(in_ch, 3*in_ch, 1),
+        act,
+        nn.Dropout(0.1),
+        nn.Convolution(3*in_ch, in_ch, 1)
+    )
+
+  def forward(self, x):
+    q, k, v = self.dw_conv(x), self.dw_conv(x), self.dw_conv(x)
+    q, k = self.pw_conv_1(q), self.pw_conv_1(k)
+    k = k.flatten(start_dim=1).T.reshape(q.shape)
+    qk = self.pw_conv_2((q * k))
+    qkv = v * qk
+    out_feat = self.pw_conv_3(qkv)
+    return x * out_feat
     
 class SelfAttentionModule(nn.Module):
   def __init__(self, embed_dim):
